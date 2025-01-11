2998b6a3eb6aa404e9dc0bf3effce1990a09e7e6
GageMcCoy12 <75149938+GageMcCoy12@users.noreply.github.com>
2025-01-11 16:40:59 -0700

    Testing again. Please forgive me for the lots of commits.

--- hooks/use-channels.ts
+++ hooks/use-channels.ts
@@ -1,7 +1,7 @@
 "use client"
 
 import { useQuery, useQueryClient } from "@tanstack/react-query"
-import { Channel } from "../../types/channels"
+import { Channel } from "../types/channels"
 
 export function useChannels() {
   const queryClient = useQueryClient()
